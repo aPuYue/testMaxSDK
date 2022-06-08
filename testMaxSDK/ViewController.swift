25b3610790b21eaa47d08c3dffff7f0b292b5a66
aPuYue <pu.yue@access-company.com>
2022-06-08 14:31:17 +0900

    1

---
+++ testMaxSDK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testMaxSDK
+//
+//  Created by Pu Yue - PU YUE on 2022/06/08.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
