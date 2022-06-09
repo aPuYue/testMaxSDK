6f85e97c30da8ca83258198be1f195f21b14a16d
aPuYue <pu.yue@access-company.com>
2022-06-09 17:12:05 +0900

    Rewarded Ads 但是界面一样

--- testMaxSDK/ViewController.swift
+++ testMaxSDK/ViewController.swift
@@ -8,71 +8,92 @@
 import UIKit
 import AppLovinSDK
 
-class ViewController: UIViewController, MAAdDelegate {
+class ViewController: UIViewController, MAAdViewAdDelegate, MARewardedAdDelegate {
+
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        createInterstitialAd()
+        createRewardedAd()
     }
 
     @IBAction func click(_ sender: Any) {
-        if interstitialAd.isReady
+        if rewardedAd.isReady
         {
-            interstitialAd.show()
+            rewardedAd.show()
         }
     }
     
-    var interstitialAd: MAInterstitialAd!
-    var retryAttempt = 0.0
-    
-    func createInterstitialAd()
-    {
-        interstitialAd = MAInterstitialAd(adUnitIdentifier: "YOUR_AD_UNIT_ID")//TODO
-        interstitialAd.delegate = self
-        
-        // Load the first ad
-        interstitialAd.load()
-    }
-    
-    // MARK: MAAdDelegate Protocol
-    
-    func didLoad(_ ad: MAAd)
-    {
-        // Interstitial ad is ready to be shown. 'interstitialAd.isReady' will now return 'true'
-        
-        // Reset retry attempt
-        retryAttempt = 0
-    }
-    
-    func didFailToLoadAd(forAdUnitIdentifier adUnitIdentifier: String, withError error: MAError)
-    {
-        // Interstitial ad failed to load
-        // We recommend retrying with exponentially higher delays up to a maximum delay (in this case 64 seconds)
-        
-        retryAttempt += 1
-        let delaySec = pow(2.0, min(6.0, retryAttempt))
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + delaySec) {
-            self.interstitialAd.load()
+    var rewardedAd: MARewardedAd!
+        var retryAttempt = 0.0
+
+        func createRewardedAd()
+        {
+            rewardedAd = MARewardedAd.shared(withAdUnitIdentifier: "YOUR_AD_UNIT_ID")
+            rewardedAd.delegate = self
+
+            // Load the first ad
+            rewardedAd.load()
         }
+
+        // MARK: MAAdDelegate Protocol
+
+        func didLoad(_ ad: MAAd)
+        {
+            // Rewarded ad is ready to be shown. '[self.rewardedAd isReady]' will now return 'YES'
+            
+            // Reset retry attempt
+            retryAttempt = 0
+        }
+
+        func didFailToLoadAd(forAdUnitIdentifier adUnitIdentifier: String, withError error: MAError)
+        {
+            // Rewarded ad failed to load
+            // We recommend retrying with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+            
+            retryAttempt += 1
+            let delaySec = pow(2.0, min(6.0, retryAttempt))
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + delaySec) {
+                self.rewardedAd.load()
+            }
+        }
+
+        func didDisplay(_ ad: MAAd) {}
+
+        func didClick(_ ad: MAAd) {}
+
+        func didHide(_ ad: MAAd)
+        {
+            // Rewarded ad is hidden. Pre-load the next ad
+            rewardedAd.load()
+        }
+
+        func didFail(toDisplay ad: MAAd, withError error: MAError)
+        {
+            // Rewarded ad failed to display. We recommend loading the next ad
+            rewardedAd.load()
+        }
+
+        // MARK: MARewardedAdDelegate Protocol
+
+    @objc(didStartRewardedVideoForAd:) func didStartRewardedVideo(for ad: MAAd) {}
+
+    @objc(didCompleteRewardedVideoForAd:) func didCompleteRewardedVideo(for ad: MAAd) {}
+
+    @objc(didRewardUserForAd:withReward:) func didRewardUser(for ad: MAAd, with reward: MAReward)
+        {
+            // Rewarded ad was displayed and user should receive the reward
+        }
+    
+    func didExpand(_ ad: MAAd) {
+        
     }
     
-    func didDisplay(_ ad: MAAd) {}
-    
-    func didClick(_ ad: MAAd) {}
-    
-    func didHide(_ ad: MAAd)
-    {
-        // Interstitial ad is hidden. Pre-load the next ad
-        interstitialAd.load()
+    func didCollapse(_ ad: MAAd) {
+        
     }
     
-    func didFail(toDisplay ad: MAAd, withError error: MAError)
-    {
-        // Interstitial ad failed to display. We recommend loading the next ad
-        interstitialAd.load()
-    }
     
 }
 
