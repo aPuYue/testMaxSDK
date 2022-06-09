9084d391db7be5266e223992b491e6d3992e418b
aPuYue <pu.yue@access-company.com>
2022-06-09 17:07:12 +0900

    成功显示Interstitial Ads

--- testMaxSDK/ViewController.swift
+++ testMaxSDK/ViewController.swift
@@ -6,14 +6,73 @@
 //
 
 import UIKit
+import AppLovinSDK
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, MAAdDelegate {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        createInterstitialAd()
     }
 
-
+    @IBAction func click(_ sender: Any) {
+        if interstitialAd.isReady
+        {
+            interstitialAd.show()
+        }
+    }
+    
+    var interstitialAd: MAInterstitialAd!
+    var retryAttempt = 0.0
+    
+    func createInterstitialAd()
+    {
+        interstitialAd = MAInterstitialAd(adUnitIdentifier: "YOUR_AD_UNIT_ID")//TODO
+        interstitialAd.delegate = self
+        
+        // Load the first ad
+        interstitialAd.load()
+    }
+    
+    // MARK: MAAdDelegate Protocol
+    
+    func didLoad(_ ad: MAAd)
+    {
+        // Interstitial ad is ready to be shown. 'interstitialAd.isReady' will now return 'true'
+        
+        // Reset retry attempt
+        retryAttempt = 0
+    }
+    
+    func didFailToLoadAd(forAdUnitIdentifier adUnitIdentifier: String, withError error: MAError)
+    {
+        // Interstitial ad failed to load
+        // We recommend retrying with exponentially higher delays up to a maximum delay (in this case 64 seconds)
+        
+        retryAttempt += 1
+        let delaySec = pow(2.0, min(6.0, retryAttempt))
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + delaySec) {
+            self.interstitialAd.load()
+        }
+    }
+    
+    func didDisplay(_ ad: MAAd) {}
+    
+    func didClick(_ ad: MAAd) {}
+    
+    func didHide(_ ad: MAAd)
+    {
+        // Interstitial ad is hidden. Pre-load the next ad
+        interstitialAd.load()
+    }
+    
+    func didFail(toDisplay ad: MAAd, withError error: MAError)
+    {
+        // Interstitial ad failed to display. We recommend loading the next ad
+        interstitialAd.load()
+    }
+    
 }
 
